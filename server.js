90b8648b9a501a846264b51ea0aee9a4947c26f5
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-12 15:42:50 +0100

    Add missing semicolons

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
 var port = process.env.PORT || 8000;
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track');
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
@@ -23,26 +23,25 @@ app.get('/', (req, res) => {
 // Not found middleware
 app.use((req, res, next) => {
     return next({ status: 404, message: 'not found' })
-})
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-    let errCode, errMessage
+    let errCode, errMessage;
 
     if (err.errors) {
         // mongoose validation error
         errCode = 400 // bad request
-        const keys = Object.keys(err.errors)
+        const keys = Object.keys(err.errors);
         // report the first validation error
-        errMessage = err.errors[keys[0]].message
+        errMessage = err.errors[keys[0]].message;
     } else {
         // generic or custom error
-        errCode = err.status || 500
-        errMessage = err.message || 'Internal Server Error'
+        errCode = err.status || 500;
+        errMessage = err.message || 'Internal Server Error';
     }
-    res.status(errCode).type('txt')
-        .send(errMessage)
-})
+    res.status(errCode).type('txt').send(errMessage);
+});
 
 app.listen(port, function () {
     console.log(`Listening to requests on http://localhost:${port}`);
