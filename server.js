6fb778e29fba6eb4dc3402015d647a24f99bc4c0
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-17 17:45:16 +0100

    change casing to follow previous implementation

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.post('/api/exercise/new-user', async (req, res, next) => {
-    const { userName } = req.body;
-    const user = new User({ username: userName });
+    const { username } = req.body;
+    const user = new User({ username: username });
 
     try {
         const newUser = await user.save();
